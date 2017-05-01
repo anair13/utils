2294a280cbf0b54442896cab11c99a9aade24c2c
Ashvin Nair <nair.ashvin@gmail.com>
2017-05-01 02:16:51 -0700

    path

---
+++ include/path.py
@@ -0,0 +1,5 @@
+import os
+
+project_dir = os.getenv("LSDC_PROJECT_DIR", "/home/ashvin/tf-poke/pokebot/baxter-poke-prediction/")
+tf_data_dir = os.getenv("LSDC_TF_DIR", "/data2/shared/tf-lsdc/tf-data/")
+poke_data_dir = os.getenv("LSDC_DATA_DIR", "/data/shared/arena/")
---
+++ scripts/watermelon_mount_drives.sh
@@ -0,0 +1,4 @@
+sudo mount /dev/sdb /data2
+sudo mount /dev/sda1 /watermelon
+sudo mount /dev/sdc /data
+
